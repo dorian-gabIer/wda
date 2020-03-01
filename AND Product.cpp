e997ad41c6facd8477a13c0f3b19901edb1d4fa2
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-03-01 14:53:17 +0100

    Create AND Product.cpp

---
+++ AND Product.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for (unsigned long long a, b; cin >> a >> b; )
+    {
+        auto c = ULLONG_MAX;
+        b ^= a;
+        do
+        {
+            b /= 2;
+            c *= 2;
+        }
+        while (b != 0);
+        cout << (a & c) << '\n';
+    }
+    return 0;
+}
